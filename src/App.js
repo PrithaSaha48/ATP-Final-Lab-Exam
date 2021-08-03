c9196dda8ff09094e5fd08b5e13359c0b4c467fc
PrithaSaha48 <sahapritha819@gmail.com>
2021-08-03 15:59:38 +0600

    Lab Task

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import"../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+import Login from "./forms/login"
+
+function App() {
+  return (
+    <div >
+      <Login />
+    </div>
+  );
+}
+
+export default App;
